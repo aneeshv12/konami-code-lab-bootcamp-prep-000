dc7fa090f994b793a79d337daf2f2cbe01ab706f
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-04 00:32:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,16 @@ const codes = [
 function init() {
   let index = 0;
   document.body.addEventListener('keydown',function(event){
-    
-  }
+    const key = event.key;
+    if(key === codes[key]){
+      index++;
+    }
+    if(index === alphabet.length){
+      alert("Finished the code!");
+      index = 0;
+    }else{
+      index = 0;
+    }
+  });
   // your code here
 }
